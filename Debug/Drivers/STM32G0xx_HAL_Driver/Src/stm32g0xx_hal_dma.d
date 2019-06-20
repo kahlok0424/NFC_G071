9e5b92f2ac238a6ad02c44b684f42274735ff90b
kahlok0424 <kahlok0424@gmail.com>
2019-06-20 16:51:14 +0800

    Created NFC project with stG071MCU

---
+++ Debug/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.d
@@ -0,0 +1,80 @@
+Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.o: \
+ ../Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Inc/stm32g0xx_hal_conf.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_rcc.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_def.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Device/ST/STM32G0xx/Include/stm32g0xx.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Device/ST/STM32G0xx/Include/stm32g071xx.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Device/ST/STM32G0xx/Include/system_stm32g0xx.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_rcc_ex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_gpio.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_gpio_ex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_dma.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_dma_ex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_cortex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_flash.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_flash_ex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_i2c.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_i2c_ex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_pwr.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_pwr_ex.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_uart.h \
+ C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_uart_ex.h
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Inc/stm32g0xx_hal_conf.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_rcc.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_def.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Device/ST/STM32G0xx/Include/stm32g0xx.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Device/ST/STM32G0xx/Include/stm32g071xx.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/core_cm0plus.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/CMSIS/Device/ST/STM32G0xx/Include/system_stm32g0xx.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_rcc_ex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_gpio.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_gpio_ex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_dma.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_dma_ex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_cortex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_flash.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_flash_ex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_i2c.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_i2c_ex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_pwr.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_pwr_ex.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_uart.h:
+
+C:/Ac6/SystemWorkbench/StudentWorkSpace/KL/NFC_G071/Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_uart_ex.h:
---
+++ Src/main.c
@@ -0,0 +1,340 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef hlpuart1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_LPUART1_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_LPUART1_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  unsigned char buffer[6];
+  buffer[0]= 0x11;
+  buffer[1]= 0x22;
+  buffer[2]= 0x33;
+  buffer[3]= 0x44;
+  buffer[4]= 0x55;
+  buffer[5]= 0x66;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  HAL_I2C_Master_Transmit(&hi2c1, (0x53<<1), buffer, 5,10);
+	  HAL_Delay(50);
+	  HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /**Configure the main internal regulator output voltage 
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Initializes the peripherals clocks 
+  */
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_LPUART1|RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.Lpuart1ClockSelection = RCC_LPUART1CLKSOURCE_PCLK1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x20303E5D;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Configure Analogue filter 
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Configure Digital filter 
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief LPUART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_LPUART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN LPUART1_Init 0 */
+
+  /* USER CODE END LPUART1_Init 0 */
+
+  /* USER CODE BEGIN LPUART1_Init 1 */
+
+  /* USER CODE END LPUART1_Init 1 */
+  hlpuart1.Instance = LPUART1;
+  hlpuart1.Init.BaudRate = 209700;
+  hlpuart1.Init.WordLength = UART_WORDLENGTH_7B;
+  hlpuart1.Init.StopBits = UART_STOPBITS_1;
+  hlpuart1.Init.Parity = UART_PARITY_NONE;
+  hlpuart1.Init.Mode = UART_MODE_TX_RX;
+  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  hlpuart1.FifoMode = UART_FIFOMODE_DISABLE;
+  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN LPUART1_Init 2 */
+
+  /* USER CODE END LPUART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
